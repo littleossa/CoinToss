f354cad9c288e8ce9de9573019d3333d3374ec65
littleossa <littleossa@gmail.com>
2023-12-14 21:00:59 +0900

    Add: Widget Extension

---
+++ CoinTossWidget/CoinTossWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  CoinTossWidgetBundle.swift
+//  CoinTossWidget
+//
+//  Created by 平岡修 on 2023/12/14.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct CoinTossWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        CoinTossWidget()
+    }
+}
